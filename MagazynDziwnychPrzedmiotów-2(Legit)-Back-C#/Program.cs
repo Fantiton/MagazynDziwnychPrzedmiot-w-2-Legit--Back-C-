9f24c40908d80f0dcaf236b8f491c599809935b4
Filip Celejewski <FilipCelejewski@wp.pl>
2025-04-22 16:16:37 +0200

    Basic interface

--- MagazynDziwnychPrzedmiotów-2(Legit)-Back-C#/Program.cs
+++ MagazynDziwnychPrzedmiotów-2(Legit)-Back-C#/Program.cs
@@ -1,2 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MagazynDziwnychPrzedmiotów_2_Legit__Back_C_;
+
+Console.WriteLine("Podaj pojemność magazynu: ");
+
+int capacity = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Podaj maksymalną wagę zawartości magazynu: ");
+
+float maxCombinedWeight = float.Parse(Console.ReadLine());
+
+Storage storage = new Storage(capacity, maxCombinedWeight);
+
+void Main()
+{
+    string input = Console.ReadLine();
+
+    switch(input){
+        case "add":
+            AddItem();
+            break;
+        case "list":
+            ListAll();
+            break;
+        }
+
+    void AddItem()
+    {
+        Console.WriteLine("Podaj nazwę przedmiotu: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Podaj wagę przedmiotu: ");
+        float weight = float.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj poziom dziwności przedmiotu: ");
+        int strangenessLevel = int.Parse(Console.ReadLine());
+        Console.WriteLine("Czy przedmiot jest kruchy? (TAK/NIE): ");
+        bool isFragile = Console.ReadLine().ToUpper() == "TAK";
+        Item item = new Item(name, weight, strangenessLevel, isFragile);
+        storage.AddItem(item);
+    }
+
+    void ListAll(){
+        storage.ListAll();
+    }
+}
