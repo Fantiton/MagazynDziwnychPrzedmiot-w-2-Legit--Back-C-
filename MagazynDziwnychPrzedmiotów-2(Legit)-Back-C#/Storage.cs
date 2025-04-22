b2e4450a468696cac07e63e016db13d5b538375d
Filip Celejewski <FilipCelejewski@wp.pl>
2025-04-22 16:04:32 +0200

    + Class Storage

---
+++ MagazynDziwnychPrzedmiotów-2(Legit)-Back-C#/Storage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazynDziwnychPrzedmiotów_2_Legit__Back_C_
+{
+    internal class Storage
+    {
+        private int Capacity;
+        private int CurrentItemCount;
+        private float MaxCombinedWeight;
+
+        public Storage(int capacity, float maxCombinedWeight)
+        {
+            Capacity = capacity;
+            MaxCombinedWeight = maxCombinedWeight;
+        }
+    }
+}
