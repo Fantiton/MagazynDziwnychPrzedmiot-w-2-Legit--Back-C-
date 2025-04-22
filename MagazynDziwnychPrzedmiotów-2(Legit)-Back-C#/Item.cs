7fd8ae436a460468e0ebf9a511d4b1a5a159a832
Filip Celejewski <FilipCelejewski@wp.pl>
2025-04-22 15:55:20 +0200

    Dodaj pliki projektów.

---
+++ MagazynDziwnychPrzedmiotów-2(Legit)-Back-C#/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazynDziwnychPrzedmiotów_2_Legit__Back_C_
+{
+    internal class Item
+    {
+        private string Name;
+        private float Weight;
+        private int StrangenessLevel;
+        private bool IsFragile;
+
+        public Item(string name, float weight, int strangenessLevel, bool isFragile)
+        {
+            Name = name;
+            Weight = weight;
+            StrangenessLevel = strangenessLevel;
+            IsFragile = isFragile;
+        }
+    }
+}
