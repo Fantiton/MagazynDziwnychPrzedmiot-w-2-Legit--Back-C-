07e4fe5d51962681ea5c3f7e2366d370e5bb5a44
Filip Celejewski <FilipCelejewski@wp.pl>
2025-04-22 16:10:35 +0200

    Storage Class: + ListAll() + GetContentWeight() + AddItem()

--- MagazynDziwnychPrzedmiotów-2(Legit)-Back-C#/Storage.cs
+++ MagazynDziwnychPrzedmiotów-2(Legit)-Back-C#/Storage.cs
@@ -11,11 +11,54 @@ namespace MagazynDziwnychPrzedmiotów_2_Legit__Back_C_
         private int Capacity;
         private int CurrentItemCount;
         private float MaxCombinedWeight;
+        private List<Item> items = new List<Item>();
 
         public Storage(int capacity, float maxCombinedWeight)
         {
             Capacity = capacity;
             MaxCombinedWeight = maxCombinedWeight;
         }
+
+        public void ListAll()
+        {
+            Console.WriteLine("Lista przedmiotów w magazynie:");
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.description());
+            }
+        }
+
+        public float GetContentWeight()
+        {
+            float totalWeight = 0;
+            foreach (var item in items)
+            {
+                totalWeight += item.GetWeight();
+            }
+            return totalWeight;
+        }
+
+        public void AddItem(Item item)
+        {
+            if (CurrentItemCount < Capacity)
+            {
+                if (GetContentWeight() + item.GetWeight() <= MaxCombinedWeight)
+                {
+                    CurrentItemCount++;
+                    Console.WriteLine($"Dodano przedmiot: {item.GetName()}");
+                }
+                else
+                {
+                    Console.WriteLine("Przedmiot jest zbyt ciężki");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Magazyn jest pełny.");
+            }
+        }
+
+
     }
 }
